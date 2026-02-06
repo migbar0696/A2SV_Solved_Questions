5811e5be54b2f2a510cfa2a24e61eaa7437ddb1b
migbar0696 <migbarubelaya@gmail.com>
2026-02-06 06:36:38 +0300

    Solved

--- Find_Duplicate_File_in_System.py
+++ Find_Duplicate_File_in_System.py
@@ -1,1 +1,29 @@
+class Solution:
+    def findDuplicate(self, paths: List[str]) -> List[List[str]]:
+        from collections import defaultdict
+        ansdict = defaultdict(list)
+        finalans = []
 
+        for path in paths:
+            pathlist = path.split()
+
+            for i in range(1, len(pathlist)):
+                flagop = False
+                strb = ''
+                stra = ''
+                for j in range(len(pathlist[i])):
+                    if pathlist[i][j] == '(':
+                        flagop = True
+
+                    if flagop and pathlist[i][j] != '('and pathlist[i][j] != ')':
+                        stra += pathlist[i][j]
+
+                    if not flagop:
+                        strb += pathlist[i][j]
+                    
+                ansdict[stra].append(pathlist[0]+"/" + strb)
+        
+        for key in ansdict.keys():
+            if len(ansdict[key]) >= 2:
+                finalans.append(ansdict[key])
+        return finalans
