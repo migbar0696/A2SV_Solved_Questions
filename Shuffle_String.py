05a6e6bada70273a6ce8b4f77e30a797c794217d
migbar0696 <migbarubelaya@gmail.com>
2026-02-10 07:14:03 +0300

    Solved

---
+++ Shuffle_String.py
@@ -0,0 +1,8 @@
+class Solution:
+    def restoreString(self, s: str, indices: List[int]) -> str:
+        n = len(s)
+        res = [""] * n
+        for i in range(n):
+            res[indices[i]] = s[i]
+
+        return "".join(res)
