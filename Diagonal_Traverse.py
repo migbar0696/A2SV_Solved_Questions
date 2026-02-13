8827e2487b7b0a96d5987965e4acb9c5b507c762
migbar0696 <migbarubelaya@gmail.com>
2026-02-13 11:55:06 +0300

    Solved

---
+++ Diagonal_Traverse.py
@@ -0,0 +1,59 @@
+class Solution:
+    def findDiagonalOrder(self, mat: List[List[int]]) -> List[int]:
+        starti = 0
+        startj = 0
+        rowl = len(mat)
+        coll = len(mat[0])
+        setindex = set()
+        res = []
+
+        for i in range(rowl):
+            for j in range(coll):
+                setindex.add((i, j))
+
+        parity = True
+
+        while (starti, startj) in setindex:
+            i = starti
+            j = startj
+            if parity:  
+                while(i, j) in setindex:
+                    res.append(mat[i][j])
+                    i -= 1
+                    j += 1
+                i += 1
+                j -= 1 
+            if not parity:
+                while(i, j) in setindex:
+                    res.append(mat[i][j])
+                    i += 1
+                    j -= 1
+
+                i -= 1
+                j += 1
+            
+            
+            if parity and j < coll - 1:
+                starti = i
+                startj = j + 1
+
+            elif parity and j >= coll - 1:
+                starti = i + 1
+                startj = j 
+
+            if not parity and i < rowl - 1:
+                starti = i + 1
+                startj = j 
+            elif not parity and i >= rowl - 1:
+                starti = i 
+                startj = j + 1
+            
+            print(starti, startj)
+
+            if parity:
+                parity = False
+            else:
+                parity = True
+
+        return res
+      
