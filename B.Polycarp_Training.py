c4afc0880e2c63e821684d14d8fbedb75af413b4
migbar0696 <migbarubelaya@gmail.com>
2026-02-19 21:18:26 +0300

    Solution for B.Polycarp_Training.py

--- B.Polycarp_Training.py
+++ B.Polycarp_Training.py
@@ -1,1 +1,10 @@
 
+n = int(input())
+contestn = sorted(list(map(int, input().split())))
+nlen = len(contestn)
+cnt = 0
+for i in range(nlen):
+    if cnt + 1 <= contestn[i]:
+        cnt += 1
+        
+print(cnt)
