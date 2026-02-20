e99a3b56cc8633d208b1652a3bf166aa988a7365
migbar0696 <migbarubelaya@gmail.com>
2026-02-20 13:41:02 +0300

    Optimized solution for Majority_Element_II.py

--- Majority_Element_II.py
+++ Majority_Element_II.py
@@ -10,3 +10,27 @@ class Solution:
                 res.append(key)
         
         return res
+
+# second solution with O(1) space
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+
+        freqele = defaultdict(int)
+        res = []
+        n = len(nums)
+
+        for num in nums:
+            freqele[num] += 1
+            
+            if len(freqele) == 3:
+                for key in list(freqele.keys()):
+                    freqele[key]  -= 1
+
+                    if freqele[key] == 0:
+                        del freqele[key]
+        for key in freqele.keys():
+            if nums.count(key)> n // 3:
+                res.append(key)
+        
+        return res
+        
