5a4e5498ca720b7c6845d44054b406a31243a1f1
migbar0696 <migbarubelaya@gmail.com>
2026-02-24 09:16:07 +0300

    Time: 55 ms (76.69%), Space: 29.7 MB (29.7%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -8,13 +8,10 @@ class Solution:
         while l < r:
             maxw = max(maxw, min(height[l] , height[r]) * (r - l))
 
-            if height[l] < height[r]:
+            if height[l] <= height[r]:
                 l += 1
             elif height[l] > height[r]:
                 r -= 1
-            elif height[l] == height[r]:
-                if height[l + 1] > height[r - 1]:
-                    l += 1
-                else:
-                    r -= 1
+ 
+
         return maxw
