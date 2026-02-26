8d42478e5b5428fdc57d987c60afe6e9e0a47cd9
migbar0696 <migbarubelaya@gmail.com>
2026-02-26 18:47:22 +0300

    Solved

---
+++ C.Number_of_Segments_with_Small_Sum.py
@@ -0,0 +1,16 @@
+n, s = map(int, input().split())
+
+listn = list(map(int, input().split()))
+n = len(listn)
+sumw = 0
+l = 0
+ans = 0
+
+for r in range(n):
+    sumw += listn[r]
+    while sumw > s:
+        sumw -= listn[l]
+        l += 1
+    ans += r - l + 1
+
+print(ans)
