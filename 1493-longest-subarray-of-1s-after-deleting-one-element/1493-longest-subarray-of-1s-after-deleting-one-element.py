137a42928eb29021a7a9da330ae1052d7378de2d
migbar0696 <migbarubelaya@gmail.com>
2026-02-26 17:18:45 +0300

    Time: 174 ms (5.03%), Space: 24.7 MB (6.94%) - LeetHub

--- 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.py
+++ 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.py
@@ -5,15 +5,13 @@ class Solution:
         l = 0
         dictb  = Counter()
         n = len(nums)
-        res = []
+
         for r in range(n):
             dictb[nums[r]] += 1
 
             while (r - l + 1 ) - dictb[1] > 1:
                 dictb[nums[l]] -= 1
-                res.append(dictb[l])
                 l += 1
 
             maxl = max(maxl, r - l )
-        print(res)
         return maxl
