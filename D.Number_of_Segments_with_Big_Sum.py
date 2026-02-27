cde88cc3607dae2f41dec5772e38744c17c4393f
migbar0696 <migbarubelaya@gmail.com>
2026-02-27 15:12:42 +0300

    Updated solution for D.Number_of_Segments_with_Big_Sum.py

--- D.Number_of_Segments_with_Big_Sum.py
+++ D.Number_of_Segments_with_Big_Sum.py
@@ -1,16 +1,34 @@
 n, s = map(int, input().split())
 
 listn = list(map(int, input().split()))
-n = len(listn)
-sumw = 0
+
+sumn = 0
+nseg = 0
 l = 0
-ans = 0
-
 for r in range(n):
-    sumw += listn[r]
-    while sumw >= s:
-        sumw -= listn[l]
+    sumn += listn[r]
+    
+    while sumn >= s:
+        nseg += n - r
+        sumn -= listn[l]
         l += 1
-    ans += r - l + 1
+print(nseg)
 
-print( (n * (n + 1) // 2) - ans)
+# or
+
+# n, s = map(int, input().split())
+
+# listn = list(map(int, input().split()))
+# n = len(listn)
+# sumw = 0
+# l = 0
+# ans = 0
+
+# for r in range(n):
+#     sumw += listn[r]
+#     while sumw >= s:
+#         sumw -= listn[l]
+#         l += 1
+#     ans += r - l + 1
+
+# print( (n * (n + 1) // 2) - ans)
