45c3a339c4c6c2edae6729ed50aa2e836c37c117
migbar0696 <migbarubelaya@gmail.com>
2026-03-17 19:30:41 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -1,49 +1,18 @@
 class Solution:
 
     def myPow(self, x: float, n: int) -> float:
-        self.pown = x
 
         def dopower(x, n):
-            if n % 2 ==  0:
-                if n < 0:
-                    if n == - 1:
-                        return 1 / self.pown
-                if n == 0:
-                    return 1
-                    
-                    y = dopower(x, n / 2)
-                    y *= y
-                    return y
-                    
-                else:
-                    if n == 1:
-                        return x
-                    
-                    if n == 0:
-                        return 1
-                    
-                    y = dopower(x, n / 2)
-                    y *= y
-                    return y
-            else:
-                if n < 0:
-                    if n == - 1:
-                        return 1 / self.pown
-                    if n == 0:
-                        return 1
-                    
-                    return dopower(x, n //2) * dopower(x, ceil(n / 2))
-                    
-                else:
-                    if n == 1:
-                        return x
-                    if n == 0:
-                        return 1
-                    
-                    return dopower(x, n //2) * dopower(x, ceil(n / 2))
-
-
-        return dopower(x, n)
-
-        
-        
+            if n < 0:
+                return 1 / dopower(x, abs(n))
+            
+            if n == 0:
+                return 1
+            
+            y = dopower(x, n//2)
+            y *= y
+            if n % 2:  
+                y *= x
+                
+            return y
+        return dopower(x,n)
