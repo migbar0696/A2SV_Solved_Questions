bbc2de63fa34591f40694e916006a5603b97ec2a
migbar0696 <migbarubelaya@gmail.com>
2026-03-25 14:54:45 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
@@ -1,11 +1,19 @@
 class Solution:
+    def __init__(self):
+        self.res = [1]
     def getRow(self, rowIndex: int) -> List[int]:
-        res = [1]
-        for i in range(1, rowIndex + 1):
-            arr = [0] * (i + 1)
-            for j in range( i ):
-                arr[j] += res[j]
-                arr[j + 1]  = res[j]
-            
-            res = arr
-        return res
+
+
+        def helper(res, rowIndex ):
+            if rowIndex == 0:
+                return self.res
+            arr =  [0] * (len(res) + 1 )
+            for i in range(len(arr) - 1):
+                arr[i] += res[i]
+                arr[i + 1] += res[i]
+            self.res = arr.copy()
+            helper(self.res, rowIndex - 1)
+            return self.res
+
+        return helper(self.res, rowIndex)
+
