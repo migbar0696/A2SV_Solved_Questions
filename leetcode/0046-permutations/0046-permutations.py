d58a794764264f7ac8e8a7013c31c2a74f7484ea
migbar0696 <migbarubelaya@gmail.com>
2026-03-26 20:47:38 +0300

    Add solution for Permutations

---
+++ leetcode/0046-permutations/0046-permutations.py
@@ -0,0 +1,21 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        ans = []
+
+        def backtrack(arr):
+            if len(arr) == len(nums):
+                ans.append(arr[:])
+                return 
+            
+
+            for i in range( len(nums) ):
+                if nums[i] not in arr:
+                    backtrack(arr + [nums[i]])
+                    # arr.append(nums[i])
+                    
+                # else:
+
+                # arr.pop()
+            
+            return ans
+        return backtrack([])
