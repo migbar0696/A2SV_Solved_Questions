be129a241a8bd57c7ffbe3fd857e1d623c736189
migbar0696 <migbarubelaya@gmail.com>
2026-04-22 05:31:36 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -8,18 +8,21 @@ class Solution:
         m = len(board)
         n = len(board[0])
 
-        visited = [[[False, 0] for i in range(n)] for j in range(m) ]
+        visited = [[False for i in range(n)] for j in range(m) ]
 
         surrounded = True
+
+        def is_edge(r, c):
+            return r == 0 or r == m - 1 or c == 0 or c == n - 1
 
         def inbound(r, c):
             return 0 <= r < len(board) and 0 <= c < len(board[0])
 
-        def dfscheck(r, c):
+        def dfs(r, c):
             # print(r,c)
             nonlocal surrounded
             
-            visited[r][c][0] = True
+            visited[r][c]= True
 
             direction = [(1, 0), (-1, 0), (0, 1), (0, -1)]
 
@@ -27,59 +30,28 @@ class Solution:
                 nrc = r + rc
                 ncc = c + cc
 
-                if inbound(nrc, ncc) and not visited[nrc][ncc][0] and board[nrc][ncc] == 'O':
-                    dfscheck(nrc, ncc)
+                if inbound(nrc, ncc) and not visited[nrc][ncc] and board[nrc][ncc] == 'O':
+                    dfs(nrc, ncc)
                 
-                elif not inbound(nrc, ncc):
-                    # print(nrc, ncc)
-                    surrounded = False
+
+
         
 
-        def dfsmarker(r, c):
-
-
-            direction = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-
-            board[r][c] = "X"
-
-            for rc, cc in direction:
-                nrc = r + rc
-                ncc = c + cc
-
-                if inbound(nrc, ncc) and board[nrc][ncc] == 'O':
-                    dfsmarker(nrc, ncc)
             
-        def dfsassure(r, c):
-
-
-            direction = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-
-            visited[r][c][1] = 1 
-
-            for rc, cc in direction:
-                nrc = r + rc
-                ncc = c + cc
-
-                if inbound(nrc, ncc) and board[nrc][ncc] == 'O' and visited[nrc][ncc][1] != 1:
-                    dfsassure(nrc, ncc)
         
 
         for i in range(m):
             for j in range(n):
-                # print(i, j)
-                # print(board[i][j])
-                if  board[i][j] == "O" and not visited[i][j][0]:
-                    # print(i,j ,'O') 
-                    dfscheck(i,j)
-                # print(surrounded)
-                if not surrounded:
-                    dfsassure(i,j)
-                if surrounded and board[i][j] == "O" and visited[i][j][1] == 0:
-                    dfsmarker(i, j)
-                # print(i,j, board)
-                # print(visited)
-                
-                surrounded = True
+
+                if board[i][j] == 'O' and is_edge(i, j):
+                    dfs(i, j)
+        
+        for i in range(m):
+            for j in range(n):
+
+                if board[i][j] == 'O' and not visited[i][j]:
+                    board[i][j] = "X"
+        
                 
         
                 
