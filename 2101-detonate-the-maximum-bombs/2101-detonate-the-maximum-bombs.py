e38ba35cd23bd974d2e90577a73406a06ebb07b0
migbar0696 <migbarubelaya@gmail.com>
2026-04-24 13:59:39 +0300

    Time: 1269 ms (14.17%), Space: 20.3 MB (50.85%) - LeetHub

---
+++ 2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.py
@@ -0,0 +1,42 @@
+class Solution:
+    def maximumDetonation(self, bombs: List[List[int]]) -> int:
+        
+        graph = defaultdict(list)
+        visited = set()
+
+        freqbomb = defaultdict(int)
+
+        for i in range(len(bombs)):
+            node = bombs[i]
+            freqbomb[tuple(node)] += 1
+            for j in range(len(bombs)):
+                nnode = bombs[j]
+                if i != j and  node[2] >= sqrt(((node[0] - nnode[0]) ** 2 )+ ((node[1] - nnode[1])**2)) :
+                    graph[tuple(node)].append(nnode)
+
+        print(freqbomb)
+        maxb = 0
+        
+        for i in range(len(bombs)):
+            queue = deque([bombs[i]])
+            visited = set([tuple(bombs[i])])
+            cnt = freqbomb[tuple(bombs[i])]
+
+            while queue:
+                node = queue.popleft()
+
+                for neighbour in graph[tuple(node)]:
+                    if tuple(neighbour) not in visited:
+                        visited.add(tuple(neighbour))
+                        queue.append(neighbour)
+                        cnt += freqbomb[tuple(neighbour)]
+                        print(node, neighbour)
+                        print(freqbomb[tuple(neighbour)])
+            
+            maxb = max(maxb, cnt)
+        print(graph)
+        return maxb
+
+
+
+
