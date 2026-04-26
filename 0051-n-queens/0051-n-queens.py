d84560a45e31d0591f8b2b937cec1bef8c24ccfa
migbar0696 <migbarubelaya@gmail.com>
2026-04-26 15:33:22 +0300

    Time: 1031 ms (5.02%), Space: 19.6 MB (70.23%) - LeetHub

--- 0051-n-queens/0051-n-queens.py
+++ 0051-n-queens/0051-n-queens.py
@@ -1,35 +1,36 @@
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
-        mat = [["."] * n for i in range(n)]
-        ans = []
-        posDiag = set()
-        negDiag = set()
-        cols = set()
+        res = []
 
-        def backtrack(row):
-            if row == n:
-                copy = ["".join(row) for row in mat]
-                ans.append(copy)
-                return 
-            
-
-            for col in range(n):
-                if col in cols or row + col  in posDiag or row - col in negDiag:
-                    continue
-                mat[row][col] = "Q"
-                posDiag.add(row + col)
-                negDiag.add(row - col)
-                cols.add(col)
-
-                backtrack(row + 1)
-
-                mat[row][col] = "."
-                posDiag.remove(row + col)
-                negDiag.remove(row - col)
-                cols.remove(col)
+        curr = [["." for i in range(n)] for j in range(n)]
         
-        backtrack(0)
-        return ans
+        # dn = [(1, 1), (1, -1), (1, 0)]
+        setld = set()
+        setrd = set()
+        setc = set()
 
+        def backtrack(row, col):
 
+            if row == n and  all("Q" in each for each in curr) :
+                res.append([''.join(each) for each in curr])
+                return
+
+            
+            for i in range(row, n):
+                for j in range(n):
+                    if i - j not in setld and i + j  not in setrd and j not in setc:
+                        setld.add(i - j)
+                        setrd.add(i + j)
+                        setc.add(j)
+                        curr[i][j] = 'Q'
+
+                        backtrack(i + 1, j)
+
+                        curr[i][j] = '.'
+                        setld.remove(i - j)
+                        setrd.remove(i + j)
+                        setc.remove(j)
+
+        backtrack(0, 0)
+        return res
 
