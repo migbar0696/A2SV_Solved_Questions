fd996944e9085a903b8099c7578e87ad6b92574d
migbar0696 <migbarubelaya@gmail.com>
2026-05-04 00:07:17 +0300

    Time: 0 ms (100%), Space: 19.2 MB (51.42%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.py
@@ -0,0 +1,12 @@
+class Solution:
+    def rotateString(self, s: str, goal: str) -> bool:
+        if len(s) != len(goal):
+            return False
+        
+        if goal in s + s:
+            return True
+        else:
+            return False
+                
+
+                
