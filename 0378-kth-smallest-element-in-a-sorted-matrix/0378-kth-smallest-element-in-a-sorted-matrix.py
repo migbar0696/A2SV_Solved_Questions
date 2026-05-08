a048cd1e210e17cc3eae070ce1929a99be4aecdc
migbar0696 <migbarubelaya@gmail.com>
2026-05-08 05:03:52 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.py
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.py
@@ -3,11 +3,19 @@ class Solution:
         
         arr = []
 
-        for each in matrix:
-            arr.extend(each)
-
-        heapify(arr)
+        n  = len(matrix)
         
-        for _ in range(k - 1):
-            heappop(arr)
-        return arr[0]
+        for i in range(n):
+            heappush(arr, (matrix[i][0], i, 0))
+
+        for _ in range(k):
+            if arr:
+                num, row, col = heappop(arr)
+                print(col, n - 1)
+        
+            if col < n - 1:
+                heappush(arr,(matrix[row][col + 1],row, col + 1))
+                
+            
+
+        return num
