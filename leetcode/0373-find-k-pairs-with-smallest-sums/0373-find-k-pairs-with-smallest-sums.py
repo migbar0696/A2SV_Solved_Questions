36f30111defc8ce8eaa8e13fd8f41fb20d62cb69
migbar0696 <migbarubelaya@gmail.com>
2026-05-08 11:50:07 +0300

    Add solution for Find K Pairs with Smallest Sums

--- leetcode/0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.py
+++ leetcode/0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.py
@@ -7,21 +7,21 @@ class Solution:
         m = len(nums2)
 
         heappush(arr, (nums1[0] + nums2[0], 0, 0))
-        visited.add((nums1[0] + nums2[0], 0, 0))
+        visited.add(( 0, 0))
         
         
 
         while len(ans) < k:
             num, col1, col2 = heappop(arr)
-            ans.append([col1, col2])
-            if col1 < n - 1 and  (nums1[col1 + 1] +  nums2[col2], col1+ 1, col2) not in visited:
-                visited.add((nums1[col1 + 1] +  nums2[col2], col1+ 1, col2))
+            ans.append([nums1[col1], nums2[col2]])
+            if col1 < n - 1 and  ( col1+ 1, col2) not in visited:
+                visited.add(( col1+ 1, col2))
                 heappush(arr, (nums1[col1 + 1] +  nums2[col2], col1+ 1, col2))
 
-            if col2 < m - 1 and (nums1[col1 ] +  nums2[col2 + 1], col1, col2 + 1) not in visited:
-                visited.add((nums1[col1 ] +  nums2[col2 + 1], col1, col2 + 1))
+            if col2 < m - 1 and (col1, col2 + 1) not in visited:
+                visited.add(( col1, col2 + 1))
                 heappush(arr, (nums1[col1 ] +  nums2[col2 + 1], col1, col2 + 1))
                 # print(visited)
-        return [ [nums1[x], nums2[y]] for x, y in ans ]
+        return ans
             
 
